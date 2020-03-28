2056dfc9a9437eba72da72cdb7799638f67dce50
Zizhou Liu <zliu16@simons-rock.edu>
2020-03-27 20:02:58 -0400

    set up done

--- project2.py
+++ project2.py
@@ -5,5 +5,33 @@ from collections import defaultdict
 import sys
 import numpy as np
 from itertools import permutations
+from stanfordnlp.server import CoreNLPClient
 
 def main(APIkey, engineID, r, t, q, k):
+    service = build("customsearch", "v1", developerKey=APIkey)
+    res = service.cse().list(q=Q, cx=searchID, num=10).execute()
+    for results in res['items']:
+        url = results['link']
+        title = results['title']
+        summary = results['snippet']
+        print('----------------------------------------')
+        print('result ' + str(count))
+        print('title: '+title)
+        print('url: '+url)
+        print('summary: '+summary)
+        #now get contents using beautiful soap
+
+        #get first 20000 characters
+
+        #use package now
+
+
+
+if __name__ == '__main__':
+    APIkey = sys.argv[1]
+    searchID = sys.argv[2]
+    r = sys.argv[3]
+    t = sys.argv[4]
+    q = sys.argv[5]
+    k = sys.argv[6]
+    main(APIkey, engineID, r, t, q, k)
