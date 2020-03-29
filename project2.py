7013589fbd33bdb787f856f063e0647e0e34f346
Zizhou Liu <zliu16@simons-rock.edu>
2020-03-29 01:34:19 -0400

    finished part1-3, debugging now

--- project2.py
+++ project2.py
@@ -11,35 +11,47 @@ import tika
 from tika import parser
 
 
-def main(APIkey, engineID, r, t, q, k):
-    X = defaultdict(dict) #key is tuple, value is confidence value, this will help with duplicates and update
+def main(APIkey, engineID, r, t, Q, k):
+    X = defaultdict(float) #key is tuple, value is confidence value, this will help with duplicates and update
+    relationdict = {1:"per_schools_attended",2:"per_employee_of",3:"per_cities_of_residence",4:"org_top_members/employees"}
 
+    # now begin searching query
     service = build("customsearch", "v1", developerKey=APIkey)
-    res = service.cse().list(q=Q, cx=searchID, num=10).execute()
+    res = service.cse().list(q=Q, cx=engineID, num=5).execute()
+    X = process_urls(res,relationdict)
+    print(X)
 
+
+def process_urls(res,relationdict):
+    X = defaultdict(float)
+    count = 1
     for results in res['items']:
         url = results['link']
         title = results['title']
-        summary = results['snippet']
         print('----------------------------------------')
         print('result ' + str(count))
         print('title: '+title)
         print('url: '+url)
-        print('summary: '+summary)
-        #now get contents using beautiful soap
-        parsed = parser.from_file(url)
+        #now get contents using tika
+        try:
+            parsed = parser.from_file(url)
+        except:
+            continue
         content = parsed["content"]
         strip_content = ' '.join(content.split())
+        print(type(strip_content))
+        print(strip_content)
         #get first 20000 characters
         if len(strip_content) >= 20000:
             strip_content = strip_content[:20000]
-        #use package now,only copied the example for now.
-        argparser = argparse.ArgumentParser()
-        argparser.add_argument("--input_text_file_path", "-i", required=True, help="Path to the text file to be parsed")
-        args = argparser.parse_args()
+        #use package now
 
-        with open(args.input_text_file_path, "r") as f:
-            text = f.read()
+        # argparser = argparse.ArgumentParser()
+        # argparser.add_argument("--input_text_file_path", "-i", required=True, help="Path to the text file to be parsed")
+        # args = argparser.parse_args()
+        #
+        # with open(args.input_text_file_path, "r") as f:
+        #     text = f.read()
 
         with CoreNLPClient(annotators=['tokenize', 'ssplit', 'pos', 'lemma', 'ner'], timeout=30000, memory='4G',
                            endpoint="http://localhost:9000") as pipeline_ner:
@@ -47,19 +59,25 @@ def main(APIkey, engineID, r, t, q, k):
                                timeout=30000, memory='4G', endpoint="http://localhost:9001") as pipeline_kbp:
                 for j in range(100):
                     print(f">>> Repeating {j}th time.")
-                    ann_ner = pipeline_ner.annotate(text)
-                    ann = pipeline_kbp.annotate(text)
+                    ann_ner = pipeline_ner.annotate(strip_content)
+                    ann = pipeline_kbp.annotate(strip_content)
                     for sentence in ann.sentence:
                         for kbp_triple in sentence.kbpTriple:
                             print(
                                 f"\t Confidence: {kbp_triple.confidence};\t "
                                 f"Subject: {kbp_triple.subject};\t Relation: {kbp_triple.relation}; "
-                                f"Object: {kbp_triple.object}")
-
-
+                                f"Object: {kbp_triple.object}") #test
+                            # extract relation
+                            if kbp_triple.relation == relationdict[r]:
+                                print("relation match") #test
+                                if X[(kbp_triple.subject,kbp_triple.relation,kbp_triple.object)] < kbp_triple.confidence\
+                                        and kbp_triple.confidence > t:#update key value now
+                                    X[(kbp_triple.subject,kbp_triple.relation,kbp_triple.object)] = kbp_triple.confidence
+                print(X.items()) #test
+    return X
 if __name__ == '__main__':
     APIkey = sys.argv[1]
-    searchID = sys.argv[2]
+    engineID = sys.argv[2]
     r = sys.argv[3]
     t = sys.argv[4]
     q = sys.argv[5]
