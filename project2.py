3ec98e4574e064ce0f352813ae0a3e850534f9d9
Samantha MacIlwaine <srm2197@columbia.edu>
2020-03-30 02:22:57 -0400

    Added almost all print statements, repetition logic works, formatting

--- project2.py
+++ project2.py
@@ -10,6 +10,7 @@ from stanfordnlp.server import CoreNLPClient
 from stanfordnlp.server import to_text
 import tika
 from tika import parser
+from decimal import *
 
 fakeX = {
     ('Mary Maxwell Gates', 'per:employee_or_member_of', 'Cascade Investment Technology'): 0.9484856396848951, 
@@ -44,26 +45,54 @@ fakeX = {
 }
 
 
+### HI GARY
+"""
+
+THINGS WE STILL NEED:
+
+- last 2 annotations (wasn't sure where to put them) which are below:
+
+print("\tExtracted kbp annotations for 17 out of total 39 sentences") <- swap with real variables
+print("\bRelations extracted from this website: 5 (Overall: 5)") <- swap with real variables
+
+"""
+
+
 def main(APIkey, engineID, r, t, Q, k):
+
+    # ignore but don't remove
+    relationdict = {'1':"per:schools_attended",'2':"per:employee_or_member_of",'3':"per:cities_of_residence",'4':"org:top_members_employees"}
+
+    # Initial print statements
+    print("\n\n____")
+    print("Parameters:")
+    print("Client key\t=", APIkey)
+    print("Engine key\t=", engineID)
+    print("Relation\t=", relationdict[r])
+    print("Threshold\t=", t)
+    print("Query\t\t=", Q)
+    print("# of Tuples\t=", k)
+    print("Loading necessary libraries; This should take a minute or so ...)")
+
+
+
     X = defaultdict(float) #key is tuple, value is confidence value, this will help with duplicates and update
     prev_query_words = set()
+    iter_count = 0
 
     while len(X) < int(k):
-        print("QUERY IS ", str(Q))
-
         #update the list of past query words
         for word in Q.split(" "):
             prev_query_words.add(word.lower())
 
         ### remove the below comment and disregard fake X (for testing only)
         #X = step3(APIkey, engineID, r, t, Q, k)
+        print("=========== Iteration:",iter_count,"- Query:",Q,"===========")
         X = fakeX
-        print("\nFAKE X: " + str(X)+ "\n")
 
         # sort X in decreasing intervals of extraction confidence
         # sorted_X is a list not dict
         sorted_X = sorted(X.items(), key=lambda item: item[1], reverse=True)
-        print("\nSORTED X: " + str(sorted_X) + "\n")
 
         # step 6: select new tuple to add to query, based on top extraction confidence level
         y = tuple()
@@ -87,18 +116,19 @@ def main(APIkey, engineID, r, t, Q, k):
         if len(y) == 0:
             break
         new_query = str(Q) + " " + y[0] + " " + y[2]
-        print("NEW QUERY: ", new_query)
         Q = new_query
+        iter_count += 1
 
     # The above loop ends when X contains at least k tuples
 
     if len(X) >= int(k):
         # step 5: return the tuples with their extraction confidence
+        print("================== ALL RELATIONS (",len(X),") =================")
+        getcontext().prec = 6
         for key, value in sorted_X:
-            print (key, value)
-            print("DONE WITH ALL")
+            print("Confidence:",Decimal(str(value)),"\t\t| Subject:", key[0],"\t\t| Object:",key[2])
     else:
-        print("NO RESULTS FOUND AT THE END")
+        print("No results found.")
     
 
 def step3(APIkey, engineID, r, t, Q, k):
@@ -107,8 +137,6 @@ def step3(APIkey, engineID, r, t, Q, k):
     service = build("customsearch", "v1", developerKey=APIkey)
     res = service.cse().list(q=Q, cx=engineID, num=5).execute()
     X = process_urls(res,relationdict,r,t)
-    print(X)
-    print("done step 3")
     return(X)
 
 def process_urls(res,relationdict,r,t):
@@ -118,10 +146,15 @@ def process_urls(res,relationdict,r,t):
     for results in res['items']:
         url = results['link']
         title = results['title']
-        print('----------------------------------------')
-        print('result ' + str(count))
-        print('title: '+title)
-        print('url: '+url)
+        print("URL (",count,"/ 10):", url)
+        print("\tFetching text from url ...")
+
+        ## gary please fill in
+        webpage_length = 0
+
+        #print('result ' + str(count))
+        #print('title: '+title)
+        #print('url: '+url)
         count += 1
         #now get contents using tika
         try:
@@ -134,7 +167,8 @@ def process_urls(res,relationdict,r,t):
         #get first 20000 characters
         if len(strip_content) >= 20000:
             strip_content = strip_content[:20000]
-        print(len(strip_content))
+        print("\tWebpage length (num characters):", len(strip_content))
+        print("\tAnnotating the webpage using [tokenize, ssplit, pos, lemma, ner] annotators ...")
         #use package now
 
         with CoreNLPClient(annotators=['tokenize', 'ssplit', 'pos', 'lemma', 'ner'], timeout=30000, memory='4G',
@@ -143,29 +177,43 @@ def process_urls(res,relationdict,r,t):
                                timeout=30000, memory='4G', endpoint="http://localhost:9001") as pipeline_kbp:
                 for j in range(10): # this number can be changed back to 10 if needed
                     try:
-                        print(f">>> Repeating {j}th time.")
+                        #print(f">>> Repeating {j}th time.")
                         ann_ner = pipeline_ner.annotate(strip_content)
-                        print("getting to annotate")
+                        #print("getting to annotate")
+
+                        print("\tExtracted", len(ann_ner.sentence), "sentences. Processing each sentence one by one to check for presence of right pair of named entity types; if so, will run the second pipeline ...")
                         for sentence in ann_ner.sentence:
-                            print("matching ners...")
+                            #print("matching ners...")
                             match = [False] * len(name_dict[r])
                             for token in sentence.token:
                                 for i in range(len(name_dict[r])):
                                     if token.ner == name_dict[r][i]:
                                         match[i] = True
                             if all(match):
-                                print("match success, begin kbp")
+                                #print("match success, begin kbp")
                                 ann = pipeline_kbp.annotate(to_text(sentence))
+                                countsentences = 0
                                 for i in ann.sentence:
+                                    if (countsentences % 5 == 0) and (countsentences != 0):
+                                        print("\tProcessed", countsentences+1,"/", len(ann_ner.sentence),"sentences")
+                                    elif (countsentences == len(ann.sentence)):
+                                        print("\tProcessed", len(ann_ner.sentence),"/", len(ann_ner.sentence),"sentences")
                                     for kbp_triple in i.kbpTriple:
                                         if kbp_triple.relation == relationdict[r]:
-                                            print("relation match")
+                                            #print("relation match")
+                                            print("\t\t=== Extracted Relation ===")
+                                            print("\t\tSentence:", i) ########## not sure if this is right, please check
+                                            print("\t\tConfidence:", kbp_triple.confidence,"; Subject:", kbp_triple.subject, "; Object:", kbp_triple.object, ";")
                                             if kbp_triple.confidence > float(t):
                                                 if X[(kbp_triple.subject,kbp_triple.relation,kbp_triple.object)] < kbp_triple.confidence:
                                                     #update key value now
                                                     X[(kbp_triple.subject,kbp_triple.relation,kbp_triple.object)] = kbp_triple.confidence
-                                                    print("inserted:")
-                                                    print((kbp_triple.subject,kbp_triple.relation,kbp_triple.object,kbp_triple.confidence))
+                                                    print("\t\tAdding to set of extracted relations")
+                                                    #print((kbp_triple.subject,kbp_triple.relation,kbp_triple.object,kbp_triple.confidence))
+                                            else:
+                                                print("\t\tConfidence is lower than threshold confidence. Ignoring this.")
+                                            print("\t\t==========")
+                                    countsentences += 1
                         break
                     except:
                         continue
