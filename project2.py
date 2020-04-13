693279cfb74f4a6e60b72c8c8a5cbfc3c9ed2ff7
Zizhou Liu <zliu16@simons-rock.edu>
2020-04-13 00:36:16 -0400

    final version

--- project2.py
+++ project2.py
@@ -1,5 +1,3 @@
-#APIKey = AIzaSyAQiB8uDcVFQ4HEXZtGfJ1YozGq3CqLWZ0
-#searchID = 018055869393845110026:w2dl6jqrm17
 from googleapiclient.discovery import build
 from collections import defaultdict
 import sys
